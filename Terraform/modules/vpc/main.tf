11d9475e32a117a5a0d2639b165d9f4869615fb4
Ricky <rs24563@email.vccs.edu>
2021-11-10 14:05:35 -0500

    Completed VPC Config for Terraform

--- Terraform/modules/vpc/main.tf
+++ Terraform/modules/vpc/main.tf
@@ -11,6 +11,7 @@ resource "aws_subnet" "PubSub1" {
     vpc_id = aws_vpc.App_Dev.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1a"
+    map_public_ip_on_launch = "true"
 
     tags = {
         Name = "Public Subnet 1"
@@ -21,6 +22,7 @@ resource "aws_subnet" "PubSub2" {
     vpc_id = aws_vpc.App_Dev.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "us-east-1b"
+    map_public_ip_on_launch = "true"
 
     tags = {
         Name = "Public Subnet 2"
@@ -45,4 +47,40 @@ resource "aws_subnet" "PrivSub2" {
     tags = {
         Name = "Private Subnet 1"
     }
+}
+
+resource "aws_internet_gateway" "Dev_IGW" {
+    vpc_id = aws_vpc.App_Dev.id
+
+    tags = {
+        Name = "Internet Gateway for Dev"
+    }
+}
+
+resource "aws_route_table" "Dev_RT" {
+    vpc_id = aws_vpc.App_Dev.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.Dev_IGW.id
+    }
+
+    tags = {
+        Name = "Dev Route Table"
+    }
+}
+
+
+resource "aws_route_table_association" "Pub1_Route" {
+    
+    subnet_id = aws_subnet.PubSub1.id
+    route_table_id = aws_route_table.Dev_RT.id
+
+}
+
+resource "aws_route_table_association" "Pub2_Route" {
+    
+    subnet_id = aws_subnet.PubSub2.id
+    route_table_id = aws_route_table.Dev_RT.id
+
 }
