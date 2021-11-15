ee2b2f0cad2d6138207d09576796061f6a5c5c91
Ricky <rs24563@email.vccs.edu>
2021-11-14 21:55:37 -0500

    Updated scripts for jenkins

--- Jenkins/Jenkins.sh
+++ Jenkins/Jenkins.sh
@@ -2,6 +2,7 @@
 #These scripts depend on the namespace 'devops-tools' to function
 #If you change the namespace, it'll need to be changed in the YAML
 
+
 function deploy_jenkins {
     
 kubectl create ns devops-tools
@@ -10,6 +11,8 @@ kubectl create -f volume.yaml
 kubectl apply -f deployment.yaml --validate=false
 kubectl apply -f service.yaml --validate=false
 }
+
+deploy_jenkins
 
 echo "Use the command 'kubectl logs -n devops-tools jenkins-### to gather the Jenkins Password"
 echo "Use http:<node ip>:32000 to log into jenkins"
